8a67bd5c572c4d5f1b73f4978b17759c5b8b0eda
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-16 15:01:22 +0000

    Deploy F-loat/ChineseBQB-client to github.com/F-loat/ChineseBQB-client.git:gh-pages

---
+++ README.md
@@ -0,0 +1,69 @@
+# ChineseBQB-client
+
+> 中国表情包大集合客户端
+
+## 运行
+
+* 克隆仓库
+
+``` sh
+git clone https://github.com/F-loat/ChineseBQB-client.git
+```
+
+* 安装依赖
+
+``` sh
+npm install
+```
+
+* 执行构建（开发模式）
+
+``` sh
+npm run dev:weapp
+```
+
+* 预览效果
+
+> 使用微信开发者工具导入项目，并将 AppID 清除
+
+## 说明
+
+* 本项目使用 [Taro](https://github.com/NervJS/taro) 框架开发
+
+* 图片资源来自 [ChineseBQB](https://github.com/zhaoolee/ChineseBQB) 项目
+
+* 由于小程序无法直接访问 GitHub，所以通过 Nginx 做了一层代理
+
+```
+server {
+  listen      443 ssl;
+  server_name proxy.youngon.com.cn;
+  
+  location / {
+    root   /usr/share/nginx/html;
+    index  index.html index.htm;
+  }
+
+  location /github/api/ {
+    proxy_pass https://api.github.com/;
+  }
+
+  location /github/raw/ {
+    proxy_pass https://raw.githubusercontent.com/;
+  }
+
+  ssl_certificate /etc/nginx/keys/proxy.youngon.com.cn.crt;
+  ssl_certificate_key /etc/nginx/keys/proxy.youngon.com.cn.key;
+}
+```
+
+## 截图
+
+<p style="vertical-align: center">
+  <img alt="首页" src="./screenshots/index.jpg" width="33%" />
+  <img alt="小程序码" src="./screenshots/qrcode.jpg" width="33%" />
+  <img alt="列表" src="./screenshots/list.jpg" width="33%" />
+  <img alt="预览" src="./screenshots/preview.jpg" width="33%" />
+  <img alt="关于" src="./screenshots/about.jpg" width="33%" />
+  <img alt="分享" src="./screenshots/share.jpg" width="33%" />
+</p>
---
+++ src/pages/list/index.tsx
@@ -0,0 +1,136 @@
+import Taro, { Component, Config } from '@tarojs/taro'
+import { View, Button, Text } from '@tarojs/components'
+import { ImageItem, parseImages } from '../../utils'
+import BQBImage from '../../components/bqb-image'
+import './index.less'
+
+interface State {
+  images: ImageItem[],
+  urls: string[]
+}
+
+interface Props { }
+
+export default class Index extends Component<Props, State> {
+  constructor(props) {
+    super(props)
+    this.state = {
+      images: [],
+      urls: []
+    }
+  }
+
+  images: ImageItem[]
+  urls: string[]
+
+  config: Config = {
+    navigationBarTitleText: '中国表情包',
+    enablePullDownRefresh: true
+  }
+
+  fetchImages = async () => {
+    Taro.showLoading({ title: '加载中' })
+
+    const { name } = this.$router.params
+
+    const { data } = await Taro.request({
+      url: `https://proxy.youngon.com.cn/github/raw/zhaoolee/ChineseBQB/master/${name}/index.md`,
+      dataType: 'text',
+      responseType: 'text'
+    })
+
+    this.images = parseImages(data)
+    this.urls = this.images.map(img => img.src)
+
+    this.showMoreImages(true)
+
+    Taro.hideLoading()
+  }
+
+  showMoreImages = (reload?: boolean) => {
+    const { images, urls } = this.state
+
+    const newImages = this.images.splice(0, 20)
+    const newUrls = this.urls.splice(0, 20)
+
+    if (reload) {
+      this.setState({
+        images: newImages,
+        urls: newUrls
+      })
+    } else {
+      this.setState({
+        images: images.concat(newImages),
+        urls: urls.concat(newUrls)
+      })
+    }
+  }
+
+  handlePreview = (src) => {
+    Taro.previewImage({
+      urls: this.state.urls,
+      current: src
+    })
+  }
+
+  randomImages = () => {
+    const allImages = this.images.concat(this.state.images)
+
+    this.images = allImages.sort(() => (Math.random() > 0.5 ? -1 : 1))
+    this.urls = this.images.map(img => img.src)
+
+    this.showMoreImages(true)
+
+    Taro.pageScrollTo({ scrollTop: 0 })
+    Taro.showToast({ title: '表情包已随机排序', icon: 'none' })
+  }
+
+  componentDidMount() {
+    const { title } = this.$router.params
+
+    if (title) {
+      Taro.setNavigationBarTitle({
+        title: decodeURIComponent(title)
+      })
+    }
+
+    this.fetchImages()
+  }
+
+  async onPullDownRefresh() {
+    await this.fetchImages()
+    Taro.stopPullDownRefresh()
+  }
+
+  onReachBottom() {
+    if (this.images.length) {
+      this.showMoreImages()
+    }
+  }
+
+  onShareAppMessage() {
+    return {
+      title: this.$router.params.title
+    }
+  }
+
+  render() {
+    const { images = [] } = this.state
+
+    return (
+      <View className='list'>
+        {images.map(img => (<View className="item" key={img.src}>
+          <BQBImage
+            src={img.src}
+            onClick={() => this.handlePreview(img.src)}
+          />
+          <Text className="item-name">{img.name}</Text>
+        </View>))}
+        <Button
+          className="flat-btn random-btn"
+          onClick={() => this.randomImages()}
+        >≈</Button>
+      </View>
+    )
+  }
+}
